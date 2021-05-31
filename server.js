fd4b0ff1000e06a4fe888b64f8b6212e684a64ad
MichaelHolding <michael.holding@students.plymouth.co.uk>
2021-05-31 14:11:11 +0100

    pre final commit, functional testing complete

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 //modules required for the application to work
-const http = require('http');
+
 const express =require('express');
-const https = require('https');
 const bodyparser = require('body-parser');
 const mysql = require('mysql');
 const request = require('request');
 const exphbs = require('express-handlebars');
 
+//////////////////////////////////////////////////////////////////////
+////////////Variables to Configure////////////////////////////////////
+//ESXi
+let vSphere_host = '192.168.0.224';
+let my_sso_password = 'Admin^123';
+let my_sso_username = 'administrator@vsphere.local';
+
+//database
+let dbHost = "Proj-mysql.uopnet.plymouth.ac.uk";
+let dbUser = "COMP3000_MHolding";
+let dbPass = "UupB551+";
+let db = "COMP3000_MHolding";
+
+//Host machine customisation options
+let cpuOptions = [2,4,8];
+let ramOptions = [4,8,16];
+//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
 //variables
 var active_vms =[];
 var template_array = [];
-var templateDetails = [];
 var session_id = '';
 const app = express();
-let my_sso_password = 'Admin^123';
-let my_sso_username = 'administrator@vsphere.local';
+
 //API ID req options
 my_http_options = {
     rejectUnauthorized: false,
@@ -44,10 +60,10 @@ app.set('view engine','hbs');
 
 //setting up connection to database
 const con = mysql.createConnection({
-    host: "Proj-mysql.uopnet.plymouth.ac.uk",
-    user: "COMP3000_MHolding",
-    password: "UupB551+",
-    database: "COMP3000_MHolding"
+    host:dbHost ,
+    user: dbUser,
+    password: dbPass,
+    database: db
 });
 con.connect(function(err){
     if(err) throw err;
@@ -70,7 +86,7 @@ app.get('/complete',function(req,res){
     res.render('complete')
 })
 app.get('/client',function (req,res){
-    res.render('client',{post:template_array});
+    res.render('client',{post:template_array, cpu:cpuOptions, ram:ramOptions});
 })
 app.get('/loading',function (req,res){
     res.render('loading');
@@ -86,7 +102,7 @@ app.get('/admin', function (req,res) {
             'vmware-api-session-id': session_id
         }
     }
-    request.get('https://192.168.0.224/rest/vcenter/vm',options,function (err, response, body){
+    request.get('https://'+vSphere_host+'/rest/vcenter/vm',options,function (err, response, body){
         if (err) throw err;
         let data = JSON.parse(body);
         console.log('status: '+ response.statusCode);
@@ -94,10 +110,10 @@ app.get('/admin', function (req,res) {
         console.log(json.length)
         for(i = 0;i< json.length; i++){
             let activeMachine = {
-                memory: json[i].memory_size_MiB,
+                memory: RAMConversion(json[i].memory_size_MiB),
                 id: json[i].vm,
                 name: json[i].name,
-                power: json[i].power_state,
+                power: powerFormating(json[i].power_state) ,
                 cpu: json[i].cpu_count
             };
             active_vms.push(activeMachine);
@@ -111,10 +127,15 @@ app.get('/admin', function (req,res) {
 app.post('/template', function (req, res) {
     let tempName = req.body.Name;
     let lib = req.body.ID;
-    addTemplate(tempName,lib);
+    let cpu = req.body.CPU;
+    let ram = req.body.RAM;
+    let hdd = req.body.HDD;
+    let os = req.body.OS;
+    addTemplate(tempName,lib,cpu,hdd,ram,os);
     template_array = [];
     populateTemplates();
-    res.redirect('/admin');
+    res.redirect('back')
+
 })
 app.post('/deploy',async function (req,res){
     console.log(req.body)
@@ -187,22 +208,24 @@ app.post('/deploy',async function (req,res){
 
         };
     }
-    await request.post('https://192.168.0.224/rest/vcenter/vm-template/library-items/' + selectedItem + '?action=deploy', options,
+    await request.post('https://'+vSphere_host+'/rest/vcenter/vm-template/library-items/' + selectedItem + '?action=deploy', options,
         function (err, response, body) {
             if(err) throw err;
             console.log(response.statusCode)
             console.log(body.value)
             if (response.statusCode ==200){
                 let vmID = body.value;
-                res.render('complete',{deployed:vmID})
-            }if (response.statusCode == 401){
+                setTimeout(function (){
+                    res.render('complete',{deployed:vmID})
+                },75000);
+
+            }else if (response.statusCode == 401){
                 res.render('problem',{message: 'There has been a problem connecting to vSphere. Please try again' +
                         'if the issue persists please contact your administrator'})
             }else{
                 res.render('problem',{message: 'There has been a critical problem connecting to vSphere.' +
                         ' Please contact your administrator'})
             }
-
         })
 })
 app.post('/getIP',function (req,res){
@@ -216,12 +239,13 @@ app.post('/getIP',function (req,res){
             'vmware-api-session-id': session_id
         }
     }
-    request.get('https://192.168.0.224/rest/vcenter/vm/'+id+'/guest/identity',options,function (err,response,body) {
+    request.get('https://'+vSphere_host+'/rest/vcenter/vm/'+id+'/guest/identity',options,function (err,response,body) {
         console.log(response.statusCode);
         if (response.statusCode ==200){
-            let ip_address = body.ip_address;
+            let ip_address = body.value.ip_address;
+            res.render('details', {ip:ip_address})
             console.log(ip_address)
-        }if (response.statusCode == 401){
+        }else if (response.statusCode == 401){
             res.render('problem',{message: 'There has been a problem connecting to vSphere. Please try again' +
                     'if the issue persists please contact your administrator'})
         }else{
@@ -230,9 +254,35 @@ app.post('/getIP',function (req,res){
         }
     })
 })
+app.post('/modifyVM', function (req,res){
+    console.log(req.body);
+    let id = req.body.id;
+    if(req.body.powerOff === 'true'){
+        powerOffVM(id);
+        res.redirect('back');
+    }else if(req.body.deleteVM === 'true'){
+        deleteVirtualMachine(req.body.id);
+        res.redirect('back');
+    }
+
+
+})
+app.post('/modifyTemplate',function(req,res){
+    console.log(req.body)
+    if(req.body.btnUpdate === 'true'){
+        updateTemplate(req.body.id,req.body.templateName,req.body.lib, req.body.cpu, req.body.storage, req.body.ram
+            ,req.body.os);
+
+
+    }else if (req.body.btnDelete === 'true'){
+        removeTemplate(req.body.id)
+        res.render('back')
+    }
+})
+//functions
 function getAuth(){
 
-    request.post('https://192.168.0.224/rest/com/vmware/cis/session',my_http_options,function (err,res,body){
+    request.post('https://'+vSphere_host+'/rest/com/vmware/cis/session',my_http_options,function (err,res,body){
         if(err) throw err;
         let json = JSON.parse(body);
         console.log(json);
@@ -240,18 +290,37 @@ function getAuth(){
         return res.statusCode;
     })
 }
-function byteToGB(value){
-    let it1 = value/1024;
-    let it2 = it1/1024;
-    let final = it2/1024;
-    return final;
+function powerOffVM(id){
+    options = {
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false,
+        headers: {
+            'vmware-api-session-id': session_id
+        }
+    }
+    request.post('https://'+vSphere_host+'/rest/vcenter/vm/'+id+'/power/stop',options,
+        function (err,response,body){
+            if(err) throw err;
+            console.log(response.statusCode)
+
+        })
 }
-function GBtoByte(value) {
-    let it1 = value * 1024;
-    let it2 = it1 * 1024;
-    let final = it2 * 1024;
-    return final;
+function deleteVirtualMachine(id){
+    options = {
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false,
+        headers: {
+            'vmware-api-session-id': session_id
+        }
+    }
+    request.delete('https://'+vSphere_host+'/rest/vcenter/vm/'+id, options, function (err,response,body){
+        console.log(response.statusCode)
+        console.log('vm deleted');
+    })
 }
+//math releated functions
 function ramGBtoMB(value){
     let final = value*1024
     return final
@@ -261,59 +330,67 @@ function RAMConversion(value){
     let final = Math.round(gb);
     return final;
 }
+//formatting functions
+function powerFormating(state){
+    let result = '';
+    if(state =="POWERED_ON"){
+        result = 'Powered ON'
+        return result;
+    }else{
+        result = 'Powered OFF';
+        return result;
+    }
+}
 
 
 
 //Database Functions
 
     function populateTemplates(){
-    let sql = "SELECT * FROM vm_template"
+        let sql = "SELECT * FROM vm_template"
         con.query(sql, function (err, result){
             if(err) throw err;
             console.log('templates recived')
             for(i=0; i <result.length; i++){
                 let data = {
+                    id: result[i].template_id,
                     name: result[i].template_name,
                     library_item: result[i].vm_item,
                     cpu:result[i].cpu,
                     storage:result[i].storage,
                     RAM:result[i].ram,
                     OS:result[i].os
-                };
+                    };
                 template_array.push(data);
-            }
+                }
             console.log(template_array);
-        })
-    }
-    function addTemplate($name, $id, $cpu,$hdd,$ram,$os){
+         })
+        }
+    function addTemplate($name, $libItem, $cpu,$hdd,$ram,$os){
         let sql = "Call AddTemplate(?,?,?,?,?,?)";
-        con.query(sql, [$name, $id,$cpu,$hdd,$ram,$os], function (err) {
+        con.query(sql, [$name, $libItem,$cpu,$hdd,$ram,$os], function (err) {
             if (err) throw err;
             console.log('Template Added');
         })
     }
-    function updateTemplate($name, $id) {
-        con.connect(function (err) {
-            if (err) throw err;
-            console.log('connected');
-            let sql = "Call UpdateTemplate (?,?)";
-            con.query(sql, [$name, $id], function (err) {
-                if (err) throw err;
-                console.log('Template updated');
-            })
-        })
+    function updateTemplate($id,$name, $libItem, $cpu,$hdd,$ram,$os) {
 
-    }
-    function removeTemplate($name) {
-        con.connect(function (err) {
+        let sql = "Call UpdateTemplate (?,?,?,?,?,?,?)";
+        con.query(sql, [$id, $name,$cpu,$hdd,$ram,$libItem,$os], function (err) {
             if (err) throw err;
-            console.log('connected');
+            console.log('Template updated');
+            template_array = [];
+            populateTemplates();
+            })
+    }
+    function removeTemplate($id) {
             let sql = "CALL RemoveTemplate(?)";
             con.query(sql, [$id], function (err) {
                 if (err) throw err;
                 console.log('Template Removed');
+                template_array = [];
+                populateTemplates();
             })
-        })
     }
 
 
